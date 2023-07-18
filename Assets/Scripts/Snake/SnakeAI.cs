00d669d312d6312a447ebdd5fd65d1d9303b60f3
jsan502 <job.santos924@gmail.com>
2023-07-18 17:12:57 +1200

    sync

--- Assets/Scripts/Snake/SnakeAI.cs
+++ Assets/Scripts/Snake/SnakeAI.cs
@@ -1,10 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SnakeAI : MonoBehaviour
 {
+    public GameObject playerFood;
+    private List<GameObject> listOfFood = new List<GameObject>();
+    private List<GameObject> listOfDecoys = new List<GameObject>();
+    private List<GameObject> listOfPossibleTargets = new List<GameObject>();
+    private SnakeMovement snakeMovement;
 
+    void Start() {
+        // Grab references.
+        listOfFood = GameObject.FindGameObjectsWithTag("Food").ToList();
+        listOfDecoys = GameObject.FindGameObjectsWithTag("Decoy").ToList();
+        listOfPossibleTargets.AddRange(listOfFood);
+        listOfPossibleTargets.AddRange(listOfDecoys);
+        listOfPossibleTargets.Add(playerFood);
+
+        // Grab components
+        snakeMovement = GetComponent<SnakeMovement>();
+    }
+
+    void Update() {
+        // Find nearest target
+        
+        // Move towards it
+    }
     // find nearest food,
     // move towards it
     
